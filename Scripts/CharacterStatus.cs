6161745754ba12c88a90b4c961e2074d888e47aa
samuraikouta <kouta_work_sa@yahoo.co.jp>
2017-07-07 01:21:08 +0900

    first commit

---
+++ Scripts/CharacterStatus.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStatus : MonoBehaviour 
+{
+	//------------攻撃の章で使用-----------------
+	//体力
+	public int HP = 100;
+	public int MaxHP = 100;
+
+	//攻撃力
+	public int Power = 10;
+
+	//最後に攻撃した対象
+	public GameObject lastAttackTarget = null;
+
+	//------------GUIおよびネットワークの章で使用----------------
+	//プレイヤー名
+	public string characterName = "Player";
+
+	//------------アニメーションの章で使用-------------------
+	//状態
+	public bool attacking = false;
+	public bool died = false;
+}
